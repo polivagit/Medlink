cb7fd7085bf6acb45a5d7f3096bcdf77b2454275
pol_o <poliva@milaifontanals.org>
2023-04-19 17:43:43 +0200

    #4 Updated Doctor INSERT scripts.

--- MySQL_Medlink/Guions/people (person)/doctor/INSERT_doctor.sql
+++ MySQL_Medlink/Guions/people (person)/doctor/INSERT_doctor.sql
@@ -4,7 +4,7 @@
 START TRANSACTION;
 USE `medlink`;
 
-INSERT INTO `medlink`.`doctor` (`doct_person_id`, `doct_collegiate_uid`, `doct_specialty_id`) VALUES (5,'01-100-AAAAA',3);
-INSERT INTO `medlink`.`doctor` (`doct_person_id`, `doct_collegiate_uid`, `doct_specialty_id`) VALUES (6,'02-200-BBBBB',8);
+INSERT INTO `medlink`.`doctor` (`doct_person_id`, `doct_collegiate_uid`, `doct_specialty_id`) VALUES (5,'01-100-AHBAA',3);
+INSERT INTO `medlink`.`doctor` (`doct_person_id`, `doct_collegiate_uid`, `doct_specialty_id`) VALUES (6,'02-200-BHBAA',8);
 
 COMMIT;
